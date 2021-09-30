24088d6c3888ea9e19cfddf3e619e14840cf63f1
Simon <Warcraft.simon@gmail.com>
2021-09-30 09:04:20 +0200

    Initial commit

---
+++ CompulsoryAssignmentPP/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CompulsoryAssignmentPP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
