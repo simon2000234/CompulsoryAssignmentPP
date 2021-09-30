11608a31284920acbc48b7c2f69563bf29e6cfdf
Simon <Warcraft.simon@gmail.com>
2021-09-30 09:36:01 +0200

    winforms

---
+++ WinFormsPrimeSearch/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormsPrimeSearch
+{
+    public partial class Form1 : Form
+    {
+        long lowInput;
+        long highInput;
+        public Form1()
+        {
+            InitializeComponent();
+            // Set the ListBox to display items in multiple columns.
+            listBox1.MultiColumn = true;
+            // Set the selection mode to multiple and extended.
+            listBox1.SelectionMode = SelectionMode.MultiExtended;
+        }
+
+        private bool CheckIfValidInput()
+        {
+            
+            try
+            {
+                lowInput = long.Parse(textBox1.Text);
+                highInput = long.Parse(textBox2.Text);
+            }
+            catch (Exception e)
+            {
+                return false;
+            }
+
+            return lowInput < highInput;
+        }
+
+        //Sequential
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(!CheckIfValidInput())
+                return;
+
+            listBox1.BeginUpdate();
+            //add The stuff here
+            listBox1.EndUpdate();
+        }
+
+        //Parallel
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (!CheckIfValidInput())
+                return;
+
+            listBox1.BeginUpdate();
+            //add The stuff here
+            listBox1.EndUpdate();
+        }
+    }
+}
